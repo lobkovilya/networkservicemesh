52086f9769f5ffe2593522c9911d78c2ea2ce20e
lobkovilya <ilya.lobkov@xored.com>
2019-05-06 17:29:39 +0700

    vpp-agent dev branch
    Signed-off-by: lobkovilya <ilya.lobkov@xored.com>

--- .mk/vpp_agent.mk
+++ .mk/vpp_agent.mk
@@ -1,7 +1,7 @@
 ARCH ?= $(shell uname -m)
 ifeq (${ARCH}, x86_64)
-  export VPP_AGENT=ligato/vpp-agent:v2.0.2
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent:v2.0.2
+  export VPP_AGENT=ligato/vpp-agent:dev
+  export VPP_AGENT_DEV=ligato/dev-vpp-agent:dev
 endif
 ifeq (${ARCH}, aarch64)
   export VPP_AGENT=ligato/vpp-agent-arm64:v2.0.2
