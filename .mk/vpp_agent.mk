66e81f078b36bd0e341f9075193a11f9571fdea7
Ilya Lobkov <ilya.lobkov@xored.com>
2019-05-15 19:12:09 +0700

    Fixed vpp 19.04
    Signed-off-by: Ilya Lobkov <ilya.lobkov@xored.com>

--- .mk/vpp_agent.mk
+++ .mk/vpp_agent.mk
@@ -1,7 +1,7 @@
 ARCH ?= $(shell uname -m)
 ifeq (${ARCH}, x86_64)
-  export VPP_AGENT=ligato/vpp-agent:dev
-  export VPP_AGENT_DEV=ligato/dev-vpp-agent:dev
+  export VPP_AGENT=lobkovilya/dev_vpp_agent
+  export VPP_AGENT_DEV=lobkovilya/dev_vpp_agent
 endif
 ifeq (${ARCH}, aarch64)
   export VPP_AGENT=ligato/vpp-agent-arm64:v2.0.2
